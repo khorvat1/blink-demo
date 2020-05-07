faa48af58057f293e08082a9de85fa95acfad08b
Kresimir Horvat <horvat.kresimir.1@gmail.com>
2020-05-07 21:24:04 +0200

    initial project commit - DB connection

---
+++ src/test/java/hr/khorvat/blink/BlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.khorvat.blink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
